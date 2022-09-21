0438be433681fa03f149df56a6f726556376794c
Stanislav Zmiev <szmiev2000@gmail.com>
2022-09-22 03:00:32 +0400

    Add the most basic types

---
+++ scripts/generate_stubs.py
@@ -0,0 +1,54 @@
+import inspect
+import typing
+from collections import deque
+from copy import deepcopy
+from pathlib import Path
+
+import tortoise.fields.base
+from tortoise import fields
+from tortoise.fields.relational import ForeignKeyField
+
+mfields = []
+for field in set(fields.__all__):
+    field_val = getattr(fields, field)
+    if (
+        inspect.isclass(field_val)
+        and issubclass(field_val, tortoise.fields.base.Field)
+        and field_val.__module__ == "tortoise.fields.data"
+    ):
+        mfields.append((field, typing.get_args(field_val.__orig_bases__[0])[0]))
+
+s = inspect.signature(tortoise.fields.base.Field.__init__)
+params = dict(s.parameters.items())
+del params["self"]
+nullable_params = deepcopy(params)
+nullable_params["null"] = nullable_params["null"].replace(annotation=typing.Literal[True])
+non_nullable_params = deepcopy(params)
+non_nullable_params["null"] = non_nullable_params["null"].replace(annotation=typing.Literal[False])
+
+deq = deque()
+p = deq.append
+p(
+    """
+import datetime
+import decimal
+import uuid
+from typing import Any, Callable, List, Literal, Optional, Union, overload
+
+import tortoise
+import tortoise.fields
+import tortoise.validators
+from tortoise.models import Model
+"""
+)
+
+for n, f in mfields:
+    p(f"@overload\ndef {n}{s.replace(parameters=non_nullable_params.values(), return_annotation=f)}: ...")
+    p(f"@overload\ndef {n}{s.replace(parameters=nullable_params.values(), return_annotation=typing.Optional[f])}: ...")
+    p(f"def {n}{s.replace(parameters=params.values(), return_annotation=inspect._empty)}: ...")
+
+p(f"\ndef ForeignKeyField{inspect.signature(ForeignKeyField).replace(return_annotation=typing.Any)}: ...")
+
+Path(__file__).parent.parent.joinpath("tortoise-stubs/__init__.pyi").write_text(
+    "\n".join(deq).replace("NoneType", "None")
+)
---
+++ tortoise-stubs/fields/__init__.pyi
@@ -0,0 +1,632 @@
+import datetime
+import decimal
+import uuid
+from typing import Any, Callable, List, Literal, Optional, Union, overload
+
+import tortoise
+import tortoise.fields
+import tortoise.validators
+from tortoise.models import Model
+
+@overload
+def BinaryField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> bytes: ...
+@overload
+def BinaryField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[bytes]: ...
+def BinaryField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def TimeField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> datetime.time: ...
+@overload
+def TimeField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[datetime.time]: ...
+def TimeField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def UUIDField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> uuid.UUID: ...
+@overload
+def UUIDField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[uuid.UUID]: ...
+def UUIDField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def DecimalField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> decimal.Decimal: ...
+@overload
+def DecimalField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[decimal.Decimal]: ...
+def DecimalField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def TextField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> str: ...
+@overload
+def TextField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[str]: ...
+def TextField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def DateField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> datetime.date: ...
+@overload
+def DateField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[datetime.date]: ...
+def DateField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def SmallIntField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> int: ...
+@overload
+def SmallIntField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[int]: ...
+def SmallIntField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def BooleanField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> bool: ...
+@overload
+def BooleanField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[bool]: ...
+def BooleanField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def FloatField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> float: ...
+@overload
+def FloatField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[float]: ...
+def FloatField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def JSONField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Union[dict, list]: ...
+@overload
+def JSONField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Union[dict, list, None]: ...
+def JSONField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def BigIntField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> int: ...
+@overload
+def BigIntField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[int]: ...
+def BigIntField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def IntField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> int: ...
+@overload
+def IntField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[int]: ...
+def IntField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def DatetimeField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> datetime.datetime: ...
+@overload
+def DatetimeField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[datetime.datetime]: ...
+def DatetimeField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def TimeDeltaField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> datetime.timedelta: ...
+@overload
+def TimeDeltaField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[datetime.timedelta]: ...
+def TimeDeltaField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+@overload
+def CharField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[False] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> str: ...
+@overload
+def CharField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: Literal[True] = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+) -> Optional[str]: ...
+def CharField(
+    source_field: Optional[str] = None,
+    generated: bool = False,
+    pk: bool = False,
+    null: bool = False,
+    default: Any = None,
+    unique: bool = False,
+    index: bool = False,
+    description: Optional[str] = None,
+    model: "Optional[Model]" = None,
+    validators: Optional[List[Union[tortoise.validators.Validator, Callable]]] = None,
+    **kwargs: Any
+): ...
+def ForeignKeyField(
+    model_name: str,
+    related_name: Union[str, None, Literal[False]] = None,
+    on_delete: str = "CASCADE",
+    db_constraint: bool = True,
+    **kwargs: Any
+) -> Any: ...
